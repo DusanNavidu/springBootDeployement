32241791e88addc2433f4f929cd051f8e1304679
ShaanFashion <navidu200210@gmail.com>
2025-08-04 10:05:53 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse72/springbootdeployement/SpringBootDeployementApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse72.springbootdeployement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeployementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeployementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/lk/ijse/gdse72/springbootdeployement/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse72.springbootdeployement;
+
+/**
+ * @author Dusan
+ * @date 8/4/2025
+ */
+
+public class CustomerController {
+}
