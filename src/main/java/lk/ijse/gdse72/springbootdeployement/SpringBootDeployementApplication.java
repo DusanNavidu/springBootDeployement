197197219b9a3e60f45964b214f57e30b1e5c7c6
ShaanFashion <navidu200210@gmail.com>
2025-08-04 10:43:03 +0530

    a

--- src/main/java/lk/ijse/gdse72/springbootdeployement/SpringBootDeployementApplication.java
+++ src/main/java/lk/ijse/gdse72/springbootdeployement/SpringBootDeployementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDeployementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDeployementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeployementApplication.class, args);
+    }
 
 }
--- src/main/java/lk/ijse/gdse72/springbootdeployement/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse72/springbootdeployement/controller/CustomerController.java
@@ -1,9 +1,19 @@
-package lk.ijse.gdse72.springbootdeployement;
+package lk.ijse.gdse72.springbootdeployement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Dusan
  * @date 8/4/2025
  */
 
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+    @GetMapping
+    public String getAllCustomers() {
+        return "List of all customers";
+    }
 }
